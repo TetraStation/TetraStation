1489871562179099f002ad3f50f9736a4e7063d4
Tamber <tamber@furryhelix.co.uk>
2020-12-27 00:49:30 +0000

    Airlock Controller variant for pod-doors, with tgui UI for same.

---
+++ tgui/packages/tgui/interfaces/Podbay.js
@@ -0,0 +1,51 @@
+import { useBackend } from '../backend';
+import { Box, Button, Flex, Section, LabeledList } from '../components';
+import { Window } from '../layouts';
+
+export const Podbay = (props, context) => {
+  const { act, data } = useBackend(context);
+  const {
+    exteriorClosed,
+    interiorClosed,
+  } = data;
+  return (
+    <Window
+      width={360}
+      height={420}>
+      <Window.Content>
+        <Section title="Airlock Status">
+          <LabeledList>
+            <LabeledList.Item label="Internal Door">
+              <Button
+                content="Close"
+                color="green"
+                onClick={() => act('close_interior', {})}
+                disabled={interiorClosed} />
+              <Button
+                content="Open"
+                color="red"
+                onClick={() => act('open_interior', {})}
+                disabled={!interiorClosed} />
+            </LabeledList.Item>
+            <LabeledList.Item label="External Door">
+              <Button
+                content="Close"
+                color="green"
+                onClick={() => act('close_exterior', {})}
+                disabled={exteriorClosed} />
+              <Button
+                content="Open"
+                color="red"
+                onClick={() => act('open_exterior', {})}
+                disabled={!exteriorClosed} />
+            </LabeledList.Item>
+          </LabeledList>
+          <Button
+            content="Cycle"
+            fluid="true"
+            onClick={() => act('cycle', {})} />
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};
